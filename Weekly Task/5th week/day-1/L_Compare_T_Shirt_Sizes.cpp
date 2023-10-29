f04b87bcb513f630fa0a17a95e327bd793311481
Md Merazul Islam <129538030+Md-Merazul-Islam@users.noreply.github.com>
2023-10-29 21:57:27 +0600

    Update L_Compare_T_Shirt_Sizes.cpp

--- Weekly Task/5th week/day-1/L_Compare_T_Shirt_Sizes.cpp
+++ Weekly Task/5th week/day-1/L_Compare_T_Shirt_Sizes.cpp
@@ -1,37 +1,88 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
 
-map<char, int> sizeMap = {{'S', 1}, {'M', 2}, {'L', 3}};
+        string a, b;
+        cin >> a >> b;
+        int m = a.size();
+        int n = b.size();
+        if (a[m - 1] == 'M' || b[n - 1]=='M')
+        {
+            if (a[m - 1] == 'L')
+            {
+                cout << ">" << endl;
+            }
+            else if (b[n - 1] == 'L')
+            {
+                cout << "<" << endl;
+            }
+            else if (a[m - 1] == 'S')
+            {
+                cout << "<" << endl;
+            }
+            else if (b[n - 1] == 'S')
+            {
+                cout << ">" << endl;
+            }
+            else
+            {
+                cout << "=" << endl;
+            }
+        }
+        else
+        {
+            if (a[m - 1] == 'S')
+            {
+                if (b[n - 1] == 'S')
+                {
 
-int compareSizes(string s1, string s2) {
-    char size1 = s1.back();
-    char size2 = s2.back();
-    int xCount1 = count(s1.begin(), s1.end(), 'X');
-    int xCount2 = count(s2.begin(), s2.end(), 'X');
-
-    if (sizeMap[size1] != sizeMap[size2]) {
-        return sizeMap[size1] < sizeMap[size2] ? -1 : 1;
-    } else if (xCount1 != xCount2) {
-        return xCount1 < xCount2 ? -1 : 1;
-    } else {
-        return 0;
+                    if (n == m)
+                    {
+                        cout << "=" << endl;
+                    }
+                    else if (m > n)
+                    {
+                        cout << "<" << endl;
+                    }
+                    else
+                    {
+                        cout << ">" << endl;
+                    }
+                }
+                else
+                {
+                    cout << "<" << endl;
+                }
+            }
+            else
+            {
+                if (b[n - 1] == 'S')
+                {
+                    cout << ">" << endl;
+                }
+                else
+                {
+                    if (m == n)
+                    {
+                        cout << "=" << endl;
+                    }
+                    else if (m > n)
+                    {
+                        cout << ">" << endl;
+                    }
+                    else
+                    {
+                        cout << "<" << endl;
+                    }
+                }
+            }
+        }
     }
-}
-
-int main() {
-    string s1, s2;
-    cin >> s1 >> s2;
-
-    int result = compareSizes(s1, s2);
-
-    if (result == 0) {
-        cout << "=" << endl;
-    } else if (result == -1) {
-        cout << "<" << endl;
-    } else {
-        cout << ">" << endl;
-    }
-
     return 0;
 }
