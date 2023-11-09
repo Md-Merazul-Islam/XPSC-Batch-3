8eebea3b1ea8e4ccdf632146b8c2191a3136c91a
Md-Merazul-Islam <mdmerazul75@gmail.com>
2023-11-09 21:29:35 +0600

    day-4

---
+++ Weekly Task/6th week/day-4/Rectangular_Tiling.cpp
@@ -0,0 +1,52 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14159265359
+#define ll long long
+#define pq priority_queue
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define pb push_back
+#define in insert
+#define vi vector <int>
+#define vpi vector<pair<int,int>>
+#define mp map<int,int>
+#define st set <int>
+#define fori for(int i=0;i<n;i++)
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repeq(i,a,b) for(int i=a;i<=b;i++)
+#define srt (v.begin(),v.end())
+#define all(v) (v.begin(),v.end())
+#define maxe *max_element
+#define mine *min_element
+#define lb lower_bound
+#define ub upper_bound
+#define nl "\n"
+#define f first
+#define s second
+#define fast  ios_base::sync_with_stdio(false),cin.tie(NULL);
+
+
+void mdmerazulislam()
+{
+
+}
+
+int main()
+{
+    fast;
+    
+    int t;cin>>t;
+    while(t--)
+    {
+
+        int a,b;cin>>a>>b;
+        if(a%2==0 || b%2==0)
+        {
+            yes;
+        }else no;
+    }
+    
+    return 0;
+}
