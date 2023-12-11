35ac93fd16a94a4917b97a7852f4be8fbfe6d8c7
Md-Merazul-Islam <mdmerazul75@gmail.com>
2023-12-11 18:04:22 +0600

    day-2

---
+++ Weekly Task/10th week Supper XPSC/day-2/D_1_Sage_s_Birthday_easy_version.cpp
@@ -0,0 +1,45 @@
+/**************بسم الله الرحمن الرحيم*************
+***********************❀‿❀************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14159265359
+#define dmin 1e-9
+#define dd double
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define vi vector<ll>
+#define vpi vector<pair<int, int>>
+#define srt(a) sort(a.begin(), a.end())
+#define rsrt(a) sort(a.rbegin(), a.rend())
+#define nl "\n"
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define loop(i, b) for (int i = 0; i < (b); ++i)
+#define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
+
+void mdmerazulislam()
+{
+}
+
+int main()
+{
+    fast;
+
+    ll n;
+    cin >> n;
+    vi a(n);
+    loop(i, n) cin >> a[i];
+    srt(a);
+    ll ans[n];
+    ll j = 0;
+    for (int i = 1; i < n; i += 2, j++)
+        ans[i] = a[j];
+    for (int i = 0; i < n; i += 2, j++)
+        ans[i] = a[j];
+    cout <<( n -1)/ 2 << endl;
+    for (ll val : ans)
+        cout << val << " ";
+    return 0;
+}
