0d6fa5aa1736716c9b5d5b1699ea0e87d7412bf0
Md-Merazul-Islam <mdmerazul75@gmail.com>
2023-12-29 20:57:26 +0600

    day-6

---
+++ Weekly Task/11th week Supper XPSC/day-6/C_Advantage.cpp
@@ -0,0 +1,62 @@
+
+/**************************************************************
+**               BISMILLAH HIR RAHMAN NIR RAHIM              **
+**                  Author: Md Merazul Islam                 **
+**                         CST, MPI                          **
+**                  Sreepur, Gazipur, Dhaka                  **
+**************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14159265359
+#define dmin 1e-9
+#define dd double
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define vi vector<ll>
+#define vpi vector<pair<ll, int>>
+#define srt(a) sort(a.begin(), a.end())
+#define rsrt(a) sort(a.rbegin(), a.rend())
+#define nn "\n"
+#define nl cout << "\n"
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define loop(i, a, b) for (int i = (a); i < (b); ++i)
+#define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
+
+void mdmerazulislam()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+       
+    }
+
+    vector<int> v1 = v;
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cout << v1[i] - (v1[i] != v[n - 1] ? v[n - 1] : v[n - 2]) << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fast;
+
+    int t;
+    cin >> t;
+    while (t--)
+        mdmerazulislam();
+
+    return 0;
+}
