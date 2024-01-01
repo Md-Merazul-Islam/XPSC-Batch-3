c070022aa6a125093f35bede46d5789fe59ecdd6
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-01-01 10:25:57 +0600

    segment tree

---
+++ Weekly Task/11th week Supper XPSC/day-6/A_Plus_or_Minus.cpp
@@ -0,0 +1,46 @@
+
+/**************************************************************
+**               BISMILLAH HIR RAHMAN NIR RAHIM              **
+**                  Author: Md Merazul Islam                 **
+**                         CST, MPI                          **
+**                  Sreepur, Gazipur, Dhaka                  **
+**************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14159265359
+#define dmin 1e-9
+#define dd double
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define vi vector<ll>
+#define vpi vector<pair<ll, int>>
+#define srt(a) sort(a.begin(), a.end())
+#define rsrt(a) sort(a.rbegin(), a.rend())
+#define nn "\n"
+#define nl cout << "\n"
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define loop(i, a, b) for (int i = (a); i < (b); ++i)
+#define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
+
+
+void mdmerazulislam()
+{
+    int a,b,c;cin>>a>>b>>c;
+    if((a+b)==c)cout<<'+'<<nn;
+    else cout<<'-'<<nn;
+
+}
+
+int main()
+{
+    fast;
+    
+    
+        int t;
+        cin >> t;
+        while (t--)mdmerazulislam();
+    
+    return 0;
+}
