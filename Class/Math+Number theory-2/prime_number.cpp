a6003f7f325567e9dcdf37f0e03d7f627ebd37fb
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-01-20 18:28:57 +0600

    math
    math

--- Class/Math+Number theory-2/prime_number.cpp
+++ Class/Math+Number theory-2/prime_number.cpp
@@ -1,32 +1,59 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef long double ld;
-#ifdef LOKAL
-#include "DEBUG_TEMPLATE.h"
-#else
-#define HERE
-#define debug(args...)
-#endif
-const int N = 2e5 +5;
-typedef pair<int,int> pii;
 
-void TEST_CASES()
+/**************************************************************
+**               BISMILLAH HIR RAHMAN NIR RAHIM              **
+**             https://github.com/Md-Merazul-Islam           **
+**************************************************************/
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define PI 3.14159265359
+#define dmin 1e-9
+#define dd double
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define vi vector<ll>
+#define vpi vector<pair<ll, ll>>
+#define pii pair<ll,ll> 
+#define srt(a) sort(a.begin(), a.end())
+#define rsrt(a) sort(a.rbegin(), a.rend())
+#define line "\n"
+#define nl cout << "\n"
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define loop(i, a, b) for (int i = (a); i < (b); ++i)
+#define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
+void print (vector<ll>&a) {loop(i,0,a.size())cout<<a[i]<<' ';}
+template <typename T> using my_ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int N = 2e5 +5;
+void hello_wrold_solve_here()
 {
-    cout<<"hello\n";
+    int n;cin>>n;
+    vector<bool>prime(n+1);
+    for (int i=2; i<n; i++) 
+    {
+        if(!prime[i]){
+            for (int j=i+i; j<n; j+=i){
+                prime[j]=true;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++)
+    {
+        if(!prime[i])cout<<i<<' ';
+    }
+    nl;
+    
+  
 }
+
 
 int32_t main()
 {
-#ifndef LOKAL
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-#endif
-    int t=1;
-    cin>>t;
-    while(t--)
-    {
-        TEST_CASES();
-    }
+    fast;
+    int t=1;cin>>t;
+    while(t--)hello_wrold_solve_here();
     return 0;
 }
